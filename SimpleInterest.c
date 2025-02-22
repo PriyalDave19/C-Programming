adb615d0ee87ffd9db3004bf25d25db2d96c3e1a
PriyalDave19 <paridave90@gmail.com>
2025-02-22 08:30:34 +0530

    Add New Fies

---
+++ SimpleInterest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int p ;
+    float r ,t, sI;
+
+    printf("Please enter a Principle,Time,Rate for the Simple Interest");
+    scanf("%d%f%f",&p,&t,&r);
+
+    sI = (p*r*t)/100;
+
+    printf("Simple Interest = %f",sI);
+}
