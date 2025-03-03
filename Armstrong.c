25c40ff5f8e10a2c5a9ee77a97ec47d71cb63be6
PriyalDave19 <paridave90@gmail.com>
2025-03-03 13:18:09 +0530

    Add New Files

--- Armstrong.c
+++ Armstrong.c
@@ -16,3 +16,25 @@ void main(){
 	else
 		printf("Number is not armstrong");
 }
+
+
+
+// #include<stdio.h>
+// void main(){
+//     int n = 153 , rem , res = 0 ,temp;
+//     temp = n;
+//     while(n > 0){
+//         rem = n % 10 ;
+//         res = (rem*rem*rem)+res; 
+//         n = n / 10;
+//     }
+//     if(temp == res){
+//         printf("Armstrong Number");
+//     }else{
+//         printf("Not a Armstrong Number");
+//     }
+// }
+
+// 1 5 3
+// 1 + 5 + 3
+//  1*1*1 + 5*5*5 + 3*3*3 = 1 + 125 + 27 = 153 ...
