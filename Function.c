f7414302445dd6f60d4387effbaa4f96f9a71e88
PriyalDave19 <paridave90@gmail.com>
2025-03-05 10:50:49 +0530

    Add New Files

---
+++ Function.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void demo();
+
+void main(){
+    demo();
+    demo();
+    demo();
+}
+void demo(){
+    printf("demo function called\n");
+}
---
+++ findmaxfunction.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void findmax();
+
+void main(){
+    findmax();
+	findmax();
+}
+void findmax(){
+	int a,b;
+    printf("enter any 2 number:\n");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    	printf("a is max\n");
+    else
+    	printf("a is max\n");
+    	
+}
